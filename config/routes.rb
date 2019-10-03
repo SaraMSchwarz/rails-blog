b55512d6a6bd5f39632b476325e1d5d12178021e
github username <saramschwarz@gmail.com>
2019-10-03 11:12:52 -0400

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   #posts index is the root
-  root 'posts#index'
+  root 'posts#index', as: 'home'
   #route to pages controller -> about view
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   resources :posts
 end
